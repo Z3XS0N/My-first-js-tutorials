74775b070dac4158b0fb5f4a58811ddda9ff7c62
Z3XS0N <124287199+Z3XS0N@users.noreply.github.com>
2023-06-21 06:36:29 +0400

    Basit Uygulama
    Uygulama Yapmak

---
+++ app.js
@@ -0,0 +1,12 @@
+// const root = document.getElementById("root").innerHTML;// divin işeriğini almak için
+
+//================= class ismini değiştirme ======================
+// const root = document.querySelector(".active");
+// var oldroorclass = root.className;
+// var newrootclass = root.className = "deactive";
+//
+// document.write(oldroorclass+`<br>`);
+// document.write(newrootclass);
+
+//================= Basit bir uygulama ========================
+//butona basınca elementin classı değişicek
