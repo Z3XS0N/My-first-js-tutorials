3d8d3c24f86e2c47a1491bb80c8bff78f3e794f5
Z3XS0N <124287199+Z3XS0N@users.noreply.github.com>
2023-06-21 23:01:47 +0400

    acsdc
    dscsdcdsc

--- app.js
+++ app.js
@@ -8,15 +8,37 @@
 // document.write(oldroorclass+`<br>`);
 // document.write(newrootclass);
 
-//================= Basit bir uygulama ========================
-//========= butona basınca elementin class name-ni değiştirme ===============
-const p = document.querySelector(".active");
-var classname = p.className;
-var number = 0;
-var output =document.getElementById("p");
-var newclassname = "deactive";
-document.getElementById("btn").onclick=function(){
-    number++;
-    var number1 = number-1;
-    output.innerHTML = "Old Class Name: "+classname+number1+"<br> New Class Name: "+newclassname+number;
-};
+//================= Basit bir uygulama 1 ========================
+//========= butona basınca body-nin classname-ni değiştirme ===============
+
+/*const body = document.querySelector(".body-active");
+const body_out = document.getElementById("body");
+
+document.getElementById("btn").onclick = function () {
+    var body_classname = body.className;
+
+    if (body_classname === "body-active") {
+        body.className = "body-deactive";
+        body_out.innerHTML = body.className;
+    } else {
+        body.className = "body-active";
+        body_out.innerHTML = body.className;
+    }
+};*/
+
+//================= formdan gelen verileri alma ============================
+
+const output = document.getElementById("p");
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Sayfanın yeniden yüklenmesini önlemek için formun varsayılan davranışını engeller
+
+    // Formdaki girdileri al
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    output.innerHTML = "Name: "+name+"<br>Email: "+email+"<br>Message:"+message;
+
+    // Formu sıfırla (isteğe bağlı)
+    // document.getElementById("myForm").reset();
+});
